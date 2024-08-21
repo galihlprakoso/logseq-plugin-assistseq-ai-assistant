76beb4d38a039a8cefa9306839ecb64e92c54202
Galih Laras Prakoso <galih.prakoso@yourpay.co.id>
2024-08-21 08:45:20 +0700

    fix release.config.js

--- release.config.js
+++ release.config.js
@@ -20,13 +20,13 @@ module.exports = {
       "@semantic-release/exec",
       {
         prepareCmd:
-          "zip -qq -r logseq-plugin-template-react-${nextRelease.version}.zip dist readme.md logo.svg LICENSE package.json",
+          "zip -qq -r logseq-plugin-assistseq-ai-assistant-${nextRelease.version}.zip dist README.md logo.png LICENSE package.json",
       },
     ],
     [
       "@semantic-release/github",
       {
-        assets: "logseq-plugin-template-react-*.zip",
+        assets: "logseq-plugin-assistseq-ai-assistant-*.zip",
       },
     ],
   ],
